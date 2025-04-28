e69b227e2c5b2c003a752424a0bd134bac4f4e2b
rasborj <rasborj@chalmers.se>
2025-04-28 09:48:41 +0200

    Completed the sound-image-sound pipeline so now both functions to create melspectograms and functions to create music from melspectograms work!

--- Image2Sound.py
+++ Image2Sound.py
@@ -2,52 +2,8 @@ import librosa
 import cv2
 import numpy as np
 import soundfile as sf
-from pydub import AudioSegment
 import os
-
-def Image2Sound(imageNameOrImage, conf):
-    
-    if type(imageNameOrImage) == str:
-        image = cv2.imread(imageNameOrImage, cv2.IMREAD_GRAYSCALE)
-        image = image.astype(np.float32) / 255.0
-        mel_db = image*80.0 - 80.0
-        mel_power = librosa.db_to_power(mel_db)
-        audio = librosa.feature.inverse.mel_to_audio(mel_power,
-                                                sr=conf.sampling_rate,
-                                                n_fft=conf.n_fft,
-                                                hop_length=conf.hop_length,
-                                                fmin=conf.fmin,
-                                                fmax=conf.fmax)
-                                                
-        return audio
-    else:
-        mel_db = image*80.0 - 80.0
-        mel_power = librosa.db_to_power(mel_db)
-        audio = librosa.feature.inverse.mel_to_audio(mel_power,
-                                                sr=conf.sampling_rate,
-                                                n_fft=conf.n_fft,
-                                                hop_length=conf.hop_length,
-                                                fmin=conf.fmin,
-                                                fmax=conf.fmax)
-        return audio
-    
-def SaveAudio(audio_array, path, filename, sr=22050):
-
-    os.makedirs(path, exist_ok=True)
-    if not filename.endswith('.mp3'):
-        filename += '.mp3'
-
-    #Save as .wav wavefile
-    temp_wav = os.path.join(path, "temp_output.wav")
-    final_mp3 = os.path.join(path, filename)
-    sf.write(temp_wav, audio_array, sr)
-
-    #Convert to mp3 and remove the saved wave
-    audio_segment = AudioSegment.from_wav(temp_wav)
-    audio_segment.export(final_mp3, format="mp3")
-    os.remove(temp_wav)
-
-
+import subprocess
 
 class conf:
     # Preprocessing settings
@@ -60,11 +16,55 @@ class conf:
     n_fft = n_mels * 20
     samples = sampling_rate * duration
 
+def Image2Sound(imageNameOrImage, conf):
+    
+    if type(imageNameOrImage) == str:
+        image = cv2.imread(imageNameOrImage, cv2.IMREAD_GRAYSCALE)
+        image = image.astype(np.float32) / 255.0
+        expected_frames = int(np.ceil((conf.samples - conf.n_fft) / conf.hop_length)) + 1
+        image = cv2.resize(image, (expected_frames, conf.n_mels))
+        mel_db = image*80.0 - 80.0
+        mel_power = librosa.db_to_power(mel_db)
+        audio = librosa.feature.inverse.mel_to_audio(mel_power,
+                                                sr=conf.sampling_rate,
+                                                n_fft=conf.n_fft,
+                                                hop_length=conf.hop_length,
+                                                fmin=conf.fmin,
+                                                fmax=conf.fmax)
+                                                
+        return audio
+    else:
+        imageNameOrImage = image.astype(np.float32) / 255.0
+        mel_db = image*80.0 - 80.0
+        mel_power = librosa.db_to_power(mel_db)
+        audio = librosa.feature.inverse.mel_to_audio(mel_power,
+                                                sr=conf.sampling_rate,
+                                                n_fft=conf.n_fft,
+                                                hop_length=conf.hop_length,
+                                                fmin=conf.fmin,
+                                                fmax=conf.fmax)
+        return audio
+    
+def SaveAudio(audio_array, path, filename, sr=44100):
+    os.makedirs(path, exist_ok=True)
+    if not filename.endswith('.mp3'):
+        filename += '.mp3'
 
+    temp_wav = os.path.join(path, "temp_output.wav")
+    final_mp3 = os.path.join(path, filename)
+
+    sf.write(temp_wav, audio_array, sr, subtype='PCM_16')
+    subprocess.run(['ffmpeg', '-i', temp_wav, '-q:a', '0', final_mp3], check=True)
+    os.remove(temp_wav)
+
+
+
+
+#Test like this, it should reproduce the example song in test_music_folder
+"""
 audio = Image2Sound("000002.jpg", conf)
-SaveAudio(audio,os.getcwd(),"testfile2.mp3")
-
-
+SaveAudio(audio,os.getcwd(),"testfile.mp3")
+"""
 
 
 
