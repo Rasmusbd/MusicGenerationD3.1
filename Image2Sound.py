eba9bb9711dcdb2f2aac50ea8dfae50cfeea11ef
rasborj <rasborj@chalmers.se>
2025-05-02 09:55:34 +0200

    Changed normalization to instead be based on a global min and maximum for each image.

--- Image2Sound.py
+++ Image2Sound.py
@@ -6,24 +6,28 @@ import os
 import subprocess
 
 class conf:
-    # Preprocessing settings
     sampling_rate = 44100
     duration = 30
-    hop_length = 694
+    samples = sampling_rate * duration
+    n_mels = 256
+    hop_length = 512 
+    n_fft = 2048 
     fmin = 20
     fmax = sampling_rate // 2
-    n_mels = 128
-    n_fft = n_mels * 20
-    samples = sampling_rate * duration
+
+def denormalize_spectrogram(norm_spectrogram, global_min=-100.00001, global_max=43.668285):
+    return norm_spectrogram * (global_max - global_min) + global_min
 
 def Image2Sound(imageNameOrImage, conf = conf):
     
     if type(imageNameOrImage) == str:
         image = cv2.imread(imageNameOrImage, cv2.IMREAD_GRAYSCALE)
+        print("Test1")
+        print(image)
         image = image.astype(np.float32) / 255.0
         expected_frames = int(np.ceil((conf.samples - conf.n_fft) / conf.hop_length)) + 1
         image = cv2.resize(image, (expected_frames, conf.n_mels))
-        mel_db = image*80.0 - 80.0
+        mel_db = denormalize_spectrogram(image)
         mel_power = librosa.db_to_power(mel_db)
         audio = librosa.feature.inverse.mel_to_audio(mel_power,
                                                 sr=conf.sampling_rate,
@@ -61,10 +65,10 @@ def SaveAudio(audio_array, path, filename, sr=44100):
 
 
 #Test like this, it should reproduce the example song in test_music_folder
-"""
+
 audio = Image2Sound("000002.jpg", conf)
 SaveAudio(audio,os.getcwd(),"testfile.mp3")
-"""
+
 
 
 
